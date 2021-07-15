6d83f8cfed189dcb1c06ee38eb2822c20a390816
Alex Korzh <alex.korzh@beat81.com>
2021-07-15 13:37:12 +0200

    Remove translations

--- SendbirdSampleTest/App.tsx
+++ SendbirdSampleTest/App.tsx
@@ -8,34 +8,17 @@
  * @format
  */
 
-import React, { useEffect, useState } from 'react'
-import { i18n } from 'i18next'
+import React from 'react'
 
-import initI18n from '~/i18n'
 import RootNavigator from '~/navigation/RootNavigator'
 import { StatusBar } from 'react-native'
-import { I18nextProvider } from 'react-i18next'
 
 const App = () => {
-  const [localI18n, setLocalI18n] = useState<i18n | null>(null)
-  const [translationIsLoaded, setTranslationIsLoaded] = useState(false)
-
-  const setupI18n = async () => {
-    const result = await initI18n(() => setTranslationIsLoaded(true))
-    setLocalI18n(result)
-  }
-
-  useEffect(() => {
-    setupI18n()
-  }, [])
-
   return (
-    !!localI18n && (
-      <I18nextProvider i18n={localI18n}>
-        <StatusBar backgroundColor='transparent' barStyle='dark-content' />
-        {translationIsLoaded && <RootNavigator />}
-      </I18nextProvider>
-    )
+    <>
+      <StatusBar backgroundColor='transparent' barStyle='dark-content' />
+      <RootNavigator />
+    </>
   )
 }
 
