96f9514ffe9c6e9b7e08bd77e4dc2cd0611ca637
Alex Korzh <alex.korzh@beat81.com>
2021-07-16 16:37:50 +0200

    Clean up app tsx

--- SendbirdSampleTest/App.tsx
+++ SendbirdSampleTest/App.tsx
@@ -1,25 +1,10 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * Generated with the TypeScript template
- * https://github.com/react-native-community/react-native-template-typescript
- *
- * @format
- */
-
 import React from 'react'
 
 import RootNavigator from '~/navigation/RootNavigator'
 import { StatusBar } from 'react-native'
 
 const App = () => {
-  return (
-    <>
-      <StatusBar backgroundColor='transparent' barStyle='dark-content' />
-      <RootNavigator />
-    </>
-  )
+  return <RootNavigator />
 }
 
 export default App
